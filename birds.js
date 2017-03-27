fe5e25d1204aaa1255561444afe9a7473cb65bba
hzj <hzjio@outlook.com>
2017-03-28 01:52:30 +0800

    test webhook

--- birds.js
+++ birds.js
@@ -9,7 +9,7 @@ router.use(function timeLog(req,res,next){
 });
 
 router.get('/',function(req,res){
-    res.send('Birds home page');
+    res.send('Birds home page after update');
 });
 
 router.get('/about',function(req,res){
