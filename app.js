307f8ba272d2603fea1888a2ef7df8bc5828240a
hzj <hzjio@outlook.com>
2017-03-27 14:09:43 +0800

    feat

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var birds = require('./birds');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use('/birds',birds);
+
+app.get('/',function(req,res){
+    res.send ('hello world!');
+});
+
+app.post('/',function(req,res){
+    res.send('Got a Post request');
+});
+
+app.put('/user',function(req,res){
+    res.send('got a put request at /user');
+});
+
+app.delete('/user',function(req,res){
+    res.send('Got a DELETE request at /user');
+});
+
+//app.route
+app.route('/book')
+.get(function(req,res){
+    res.send('Get a random book');
+})
+.post(function(req,res){
+    res.send('add a book');
+})
+.put(function(req,res){
+    res.send('update the book');
+});
+
+var server = app.listen(3000,function(){
+    var port = server.address().port;
+    var host = server.address().host;
+
+    console.log('express app listening ad http://%s:%s',host,port);
+});
---
+++ birds.js
@@ -0,0 +1,21 @@
+//rotuer中间件路由系统
+
+var express = require('express');
+var router = express.Router();
+
+router.use(function timeLog(req,res,next){
+    console.log('time:',Date.now());
+    next();
+});
+
+router.get('/',function(req,res){
+    res.send('Birds home page');
+});
+
+router.get('/about',function(req,res){
+    res.send('about birds');
+});
+
+
+
+module.exports = router;  
